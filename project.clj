8b5963d5c94f376bab85b05e1f92fa78b0ccfff2
Gal Dolber <gal.dolber@gmail.com>
2014-06-18 00:06:01 +0200

    12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject appengy "0.1.11"
+(defproject appengy "0.1.12"
   :description "Appengy app server"
   :url "http://github.com/galdolber/appengy"
   :main appengy.httpkit.Httpkit
@@ -8,9 +8,9 @@
   :plugins [[lein-deps-tree "0.1.2"]
             [lein-kibit "0.0.8"]
             [lein-catnip "0.5.1"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.2.0-beta2"]
-                 [http-kit "2.1.1"]
+                 [http-kit "2.1.18"]
                  [org.clojure/tools.reader "0.7.4"]
                  [org.clojure/tools.logging "0.2.3"]
                  [ch.qos.logback/logback-classic "1.0.11"]
